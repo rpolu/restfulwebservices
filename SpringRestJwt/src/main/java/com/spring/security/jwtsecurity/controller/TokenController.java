a04fb54c67ab2c2a9ac826e5b59473775077f7dd
rpolu <rpolu@10.127.26.57>
2019-12-06 16:49:31 +0530

    committed for jwt gir

---
+++ SpringRestJwt/src/main/java/com/spring/security/jwtsecurity/controller/TokenController.java
@@ -0,0 +1,27 @@
+package com.spring.security.jwtsecurity.controller;
+
+import com.spring.security.jwtsecurity.model.JwtUser;
+import com.spring.security.jwtsecurity.security.JwtGenerator;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/token")
+public class TokenController {
+
+
+    private JwtGenerator jwtGenerator;
+
+    public TokenController(JwtGenerator jwtGenerator) {
+        this.jwtGenerator = jwtGenerator;
+    }
+
+    @PostMapping
+    public String generate(@RequestBody final JwtUser jwtUser) {
+
+        return jwtGenerator.generate(jwtUser);
+
+    }
+}
