8f5f4becdb72e7f2d4a6e8b8b9315357cd40edd7
rpolu <rpolu@192.168.1.100>
2020-07-08 08:00:43 +0530

    committed

--- springrestwithoutboot/src/main/java/com/elegant/rest/controller/CountryController.java
+++ springrestwithoutboot/src/main/java/com/elegant/rest/controller/CountryController.java
@@ -22,23 +22,23 @@ public class CountryController {
 		this.countryService = countryService;
 	}
 
-	@RequestMapping(value = "/countries", method = RequestMethod.GET, headers = "Accept=application/json")
+	@RequestMapping(value = "/countries", method = RequestMethod.GET)
 	public List<Country> getCountries() {
 		List<Country> listOfCountries = countryService.getAllCountries();
 		return listOfCountries;
 	}
 
-	@RequestMapping(value = "/country/{id}", method = RequestMethod.GET, headers = "Accept=application/json")
+	@RequestMapping(value = "/country/{id}", method = RequestMethod.GET)
 	public Country getCountryById(@PathVariable int id) {
 		return countryService.getCountry(id);
 	}
 
-	@RequestMapping(value = "/countries", method = RequestMethod.POST, headers = "Accept=application/json")
+	@RequestMapping(value = "/countries", method = RequestMethod.POST)
 	public Country addCountry(@RequestBody Country country) {
 		return countryService.addCountry(country);
 	}
 
-	@RequestMapping(value = "/countries", method = RequestMethod.PUT, headers = "Accept=application/json")
+	@RequestMapping(value = "/countries", method = RequestMethod.PUT)
 	public Country updateCountry(@RequestBody Country country) {
 		return countryService.updateCountry(country);
 
